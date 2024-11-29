fb541a5fc89c26ce6779d92d17ad0649e97f4ede
adrianh-za <5121135+adrianh-za@users.noreply.github.com>
2024-11-29 11:55:18 +0000

    Update

--- iac/container_resources.tf
+++ iac/container_resources.tf
@@ -1,7 +1,7 @@
 ﻿
 resource "azurerm_container_app_environment" "formula-one" {
   location                    = azurerm_resource_group.formula-one.location
-  name                        = "f1appenv${var.env_id}"
+  name                        = "f1env${var.env_id}"
   resource_group_name         = azurerm_resource_group.formula-one.name
   log_analytics_workspace_id  = azurerm_log_analytics_workspace.formula-one.id
 
@@ -13,7 +13,7 @@ resource "azurerm_container_app_environment" "formula-one" {
 
 resource "azurerm_container_registry" "formula-one" {
   location                      = azurerm_resource_group.formula-one.location
-  name                          = "f1contreg${var.env_id}"
+  name                          = "f1reg${var.env_id}"
   resource_group_name           = azurerm_resource_group.formula-one.name
   sku                           = "Basic"
   admin_enabled                 = true
@@ -26,7 +26,7 @@ resource "azurerm_container_registry" "formula-one" {
 }
 resource "azurerm_container_app" "formula-one" {
   container_app_environment_id  = azurerm_container_app_environment.formula-one.id
-  name                          = "f1contapp${var.env_id}"
+  name                          = "f1app${var.env_id}"
   resource_group_name           = azurerm_resource_group.formula-one.name
   revision_mode                 = "Multiple"
 
@@ -37,7 +37,7 @@ resource "azurerm_container_app" "formula-one" {
     container {
       cpu    = 0.25
       memory = "0.5Gi"
-      name   = "f1-api-${var.env_id}"
+      name   = "f1api${var.env_id}"
       image  = "mcr.microsoft.com/k8se/quickstart:latest"
     }
   }
--- iac/logging_analytics_workspace.tf
+++ iac/logging_analytics_workspace.tf
@@ -1,6 +1,6 @@
 ﻿resource "azurerm_log_analytics_workspace" "formula-one" {
   location            = azurerm_resource_group.formula-one.location
-  name                = "f1law${var.env_id}"
+  name                = "f1log${var.env_id}"
   resource_group_name = azurerm_resource_group.formula-one.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
